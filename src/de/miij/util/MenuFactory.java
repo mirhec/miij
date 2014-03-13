00f36c7f1cd029a84df28217a6696d0f37335063
Mirko Hecky <mirhec@nochains.de>
2014-03-13 16:08:38 +0100

    MenuFactory extended

--- src/de/miij/util/MenuFactory.java
+++ src/de/miij/util/MenuFactory.java
@@ -47,4 +47,74 @@ public class MenuFactory {
 		i.setAccelerator(KeyStroke.getKeyStroke(shortcut));
 		return i;
 	}
+
+	/**
+	 * Create a new checkbox menu item with an ActionListener.
+	 * The title can contain a '&' character to identify the mnemonic
+	 * char.
+	 *
+	 * @param title
+	 * @param a
+	 * @return
+	 */
+	public static JCheckBoxMenuItem createCheckBoxItem(String title, ActionListener a) {
+		JCheckBoxMenuItem i = new JCheckBoxMenuItem(title.replaceAll("&", ""));
+		i.addActionListener(a);
+		int index = -1;
+		if ((index = title.indexOf("&")) > -1)
+			i.setMnemonic(title.charAt(index + 1));
+		return i;
+	}
+
+	/**
+	 * Create a new checkbox menu item with an ActionListener.
+	 * The title can contain a '&' character to identify the mnemonic
+	 * char.
+	 *
+	 * @param title
+	 * @param shortcut    Shortcut as String, e.g. "control N" or "alt shift X" or "INSERT"
+	 *                    For more details see {@link javax.swing.KeyStroke#getKeyStroke(String)}
+	 * @param a
+	 * @return
+	 */
+	public static JCheckBoxMenuItem createCheckBoxItem(String title, String shortcut, ActionListener a) {
+		JCheckBoxMenuItem i = createCheckBoxItem(title, a);
+		i.setAccelerator(KeyStroke.getKeyStroke(shortcut));
+		return i;
+	}
+
+	/**
+	 * Create a new radio button menu item with an ActionListener.
+	 * The title can contain a '&' character to identify the mnemonic
+	 * char.
+	 *
+	 * @param title
+	 * @param a
+	 * @return
+	 */
+	public static JRadioButtonMenuItem createRadioButtonItem(String title, ActionListener a) {
+		JRadioButtonMenuItem i = new JRadioButtonMenuItem(title.replaceAll("&", ""));
+		i.addActionListener(a);
+		int index = -1;
+		if ((index = title.indexOf("&")) > -1)
+			i.setMnemonic(title.charAt(index + 1));
+		return i;
+	}
+
+	/**
+	 * Create a new radio button menu item with an ActionListener.
+	 * The title can contain a '&' character to identify the mnemonic
+	 * char.
+	 *
+	 * @param title
+	 * @param shortcut    Shortcut as String, e.g. "control N" or "alt shift X" or "INSERT"
+	 *                    For more details see {@link javax.swing.KeyStroke#getKeyStroke(String)}
+	 * @param a
+	 * @return
+	 */
+	public static JRadioButtonMenuItem createRadioButtonItem(String title, String shortcut, ActionListener a) {
+		JRadioButtonMenuItem i = createRadioButtonItem(title, a);
+		i.setAccelerator(KeyStroke.getKeyStroke(shortcut));
+		return i;
+	}
 }
